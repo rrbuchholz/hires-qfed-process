37a2bf45345ae50516a28345964b9eb3418dc59d
rrbuchholz <buchholz@ucar.edu>
2024-04-22 16:53:30 -0600

    Troubleshooting batch script

--- batch_redistribute_emiss.sh
+++ batch_redistribute_emiss.sh
@@ -6,7 +6,8 @@
 # data content due to high resolution (0.1 degree)
 #-------------------
 
-arr=(OC BC SO4 VBS SOAG)       # options
+#arr=(OC BC SO4 VBS SOAG)       # options
+arr=(VBS SOAG)       # options
 #arr=(OC BC SO4 VBS SOAG SOAG1.5)       # options
 year_arr=(2023)     # options
 echo ${arr[@]}
@@ -23,16 +24,16 @@ for y in ${year_arr[@]} ; do
         if [[ ${x} == "OC" || ${x} == "BC" ]]; then
           echo "----------------${x}"
           for z in {1..4}; do
-            echo "ncl 'tracer="${x}"' 'outres = "hires"' 'year="${y}"' 'PROCESSNUM=${z}' redistribute_emiss_hires.ncl"
-            ncl 'tracer="'${x}'"' 'outres = "hires"' 'year="'${y}'"' 'PROCESSNUM="'${z}'"' redistribute_emiss_hires.ncl
+            echo "ncl 'tracer="${x}"' 'outres="hires"' 'year="${y}"' 'PROCESSNUM=${z}' redistribute_emiss_hires.ncl"
+            ncl 'tracer="'${x}'"' 'outres="hires"' 'year="'${y}'"' 'PROCESSNUM="'${z}'"' redistribute_emiss_hires.ncl
           done
 
         # 3 files for SO4
         elif [[ ${x} == "SO4" ]]; then 
           echo "----------------${x}"
           for z in {1..3}; do
-            echo "ncl 'tracer="${x}"' 'outres = "hires"' 'year="${y}"' 'PROCESSNUM=${z}' redistribute_emiss_hires.ncl"
-            ncl 'tracer="'${x}'"' 'outres = "hires"' 'year="'${y}'"' 'PROCESSNUM="'${z}'"' redistribute_emiss_hires.ncl
+            echo "ncl 'tracer="${x}"' 'outres="hires"' 'year="${y}"' 'PROCESSNUM=${z}' redistribute_emiss_hires.ncl"
+            ncl 'tracer="'${x}'"' 'outres="hires"' 'year="'${y}'"' 'PROCESSNUM="'${z}'"' redistribute_emiss_hires.ncl
           done
 
         # Subset files for IVOC then 1 file, 1 file for SVOC
@@ -40,20 +41,20 @@ for y in ${year_arr[@]} ; do
           for z in {1..2}; do
             if [[ ${z} == 1 ]]; then
               for s in SUBSETA SUBSETB; do
-      	        echo "ncl 'tracer="${x}"' 'outres = "hires"' 'year="${y}"' 'PROCESSNUM=${z}' '"${s}"="True"' redistribute_emiss_hires.ncl"
-                ncl 'tracer="'${x}'"' 'outres = "hires"' 'year="'${y}'"' 'PROCESSNUM="'${z}'"' '"${s}"="True"' redistribute_emiss_hires.ncl
+      	        echo "ncl 'tracer="${x}"' 'outres="hires"' 'year="${y}"' 'PROCESSNUM=${z}' '"${s}"="True"' redistribute_emiss_hires.ncl"
+                ncl 'tracer="'${x}'"' 'outres="hires"' 'year="'${y}'"' 'PROCESSNUM="'${z}'"' ''"${s}"'=True' redistribute_emiss_hires.ncl
               done
-            else
-      	        echo "ncl 'tracer="${x}"' 'outres = "hires"' 'year="${y}"' 'PROCESSNUM=${z}' redistribute_emiss_hires.ncl"
-                ncl 'tracer="'${x}'"' 'outres = "hires"' 'year="'${y}'"' 'PROCESSNUM="'${z}'"' redistribute_emiss_hires.ncl
+            elif [[ ${z} == 2 ]]; then
+      	        echo "ncl 'tracer="${x}"' 'outres="hires"' 'year="${y}"' 'PROCESSNUM=${z}' redistribute_emiss_hires.ncl"
+                ncl 'tracer="'${x}'"' 'outres="hires"' 'year="'${y}'"' 'PROCESSNUM="'${z}'"' redistribute_emiss_hires.ncl
             fi
           done
 
         # 1 file for SOAG
         elif [[ ${x} == "SOAG" ]]; then
           echo "----------------${x}"
-      	  echo "ncl 'tracer="${x}"' 'outres = "hires"' 'year="${y}"' redistribute_emiss_hires.ncl"
-          ncl 'tracer="'${x}'"' 'outres = "hires"' 'year="'${y}'"' redistribute_emiss_hires.ncl
+      	  echo "ncl 'tracer="${x}"' 'outres="hires"' 'year="${y}"' redistribute_emiss_hires.ncl"
+          ncl 'tracer="'${x}'"' 'outres="hires"' 'year="'${y}'"' redistribute_emiss_hires.ncl
 
         else
             echo "skipping"
